63fd9de5c16fe2a0fb6c08e3fe03e5c08e97a8fb
Jake Bolton <ninmonkeys@gmail.com>
2024-05-30 18:34:22 -0500

    Cleanup Old Launcher, Create new Examples file to import the module

---
+++ Examples/Merging Queries as One Library.ps1
@@ -0,0 +1,27 @@
+﻿$impo_Splat = @{ ErrorAction = 'stop' ; PassThru = $true }
+$ModulePath = Get-Item 'H:\data\2024\tabular\Ninmonkey.PowerQueryLib\source-pwsh\Nin.PqLib.psd1'
+@(
+    Import-module @impo_splat Pansies
+    Import-module @impo_splat -Force $ModulePath
+) | Join-String -p { $_.Name, $_.Version } -sep ', '
+
+$Config = @{
+    AppRoot = ($AppRoot = Get-Item $PSScriptRoot)
+    ImportRoot = Join-Path $AppRoot '../source'
+}
+$Color = @{
+    Green = '#47c589'
+    Fg    = '#47c589'
+}
+
+function FindPowerQuerySources {
+    param( [string]$RootDir )
+    Get-ChildItem -path *.pq -Recurse
+}
+$all_pq_files = FindPowerQuerySources -RootDir $Config.ImportRoot
+
+$Config | Ft -auto
+'Found {0} .pq files under the root path: "{1}"' -f @(
+    $all_pq_files.count
+    $Config.ImportRoot | Get-Item
+) | Write-host -fg $Color.Fg
