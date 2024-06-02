1fb2ce26619529995e8e0d99470cb2581e70f00d
Jake Bolton <ninmonkeys@gmail.com>
2024-06-02 12:58:20 -0500

    New: Script auto-exports a name based on the date: //build/auto-export/ Update: Find-PqLibSources: Partial implementation that allows more regex options for (#6) and (#7)

--- Examples/Merging Queries as One Library.ps1
+++ Examples/Merging Queries as One Library.ps1
@@ -1,13 +1,15 @@
 ﻿$impo_Splat = @{ ErrorAction = 'stop' ; PassThru = $true }
 $ModulePath = Get-Item 'H:\data\2024\tabular\Ninmonkey.PowerQueryLib\source-pwsh\Nin.PqLib.psd1'
+$AppRoot    = Get-Item -ea 'stop' $PSScriptRoot
 @(
     Import-module @impo_splat Pansies
     Import-module @impo_splat -Force $ModulePath
 ) | Join-String -p { $_.Name, $_.Version } -sep ', '
 
 $Config = @{
-    AppRoot = ($AppRoot = Get-Item $PSScriptRoot)
-    ImportRoot = Join-Path $AppRoot '../source'
+    AppRoot = $AppRoot
+    ImportRoot = Join-Path $AppRoot '../source' | Get-Item -ea 'stop'
+    AutoExportRoot = Join-path $AppRoot '../build/auto-export' | Get-Item -ea 'stop'
 }
 $Color = @{
     Fg    = '#47c589'
@@ -25,6 +27,12 @@ $select_pq = Find-PqLibSources -RootDir $Config.ImportRoot -Regex $RegexMustMatc
 'Final Files' | write-host -fg $Color.H1
 $select_pq -join "`n"
 
+$dynamicName = Join-String -f '{0}.ninlib.pq' -In (get-date).tostring('yyyy-MM-dd')
+$newExport = Join-path $Config.AutoExportRoot $DynamicName
+hr
+gc ($select_pq | select -First 1) | sc -Path $newExport
+$newExport = $newExport | Get-Item
+'wrote: {0}' -f $NewExport | write-host -fg $Color.Red
 
 return
 
--- source-pwsh/Nin.PqLib.psm1
+++ source-pwsh/Nin.PqLib.psm1
@@ -1,6 +1,103 @@
 using namespace System.Collections.Generic
 #Requires -Version 7
 
+Import-Module Pansies
+
+$script:Color = @{
+    Fg    = '#47c589'
+    Green = '#47c589'
+    H1    = '#fea9aa'
+    Red   = '#fea9aa'
+}
+
+
+function Find-PqLibSources {
+    <#
+    .SYNOPSIS
+        find files, summarize the counts. You can filter by regex
+    .example
+
+    #>
+    [Alias('Find-PqSources')]
+    [OutputType( [System.IO.FileInfo[]] )]
+    param(
+        # Root directory to search
+        [Parameter(Mandatory)]
+        [string]$RootDir,
+
+        # regex to match against
+        [Alias('Patterns')]
+        [string[]]$Regex,
+
+        # Like -Regex but this matches the end of the base file name (ignoring extension )
+        [Alias('RegexStart')]
+        [string[]]$StartsWith,
+
+        # Like -Regex but this matches the start of the base file name)
+        [Alias('RegexEnd')]
+        [string[]]$EndsWith,
+
+        # Filetype
+        [ArgumentCompletions( "'*.pq'", "'*.m'", "'*.powerquery'" )]
+        [string]$GciFilter = '*.pq'
+    )
+    $ResolvedRoot  = Get-Item -ea 'stop' $RootDir
+    $query = Get-ChildItem -path $ResolvedRoot $GciFilter -Recurse
+
+    'Found {0} .pq files under the root path: "{1}"' -f @(
+        $query.count
+        $ResolvedRoot
+    ) | Write-host -fg $Color.Fg
+
+    $WasPassedAnyRegexParam = $Regex.count -gt 0 -or $StartsWith.Count -gt 0 -or $EndsWith.count -gt 0
+    $orig = $query
+
+    if( $WasPassedAnyRegexParam ) {
+
+        write-warning 'partial match not quite working'
+        [pscustomobject]@{
+            ReCount    = $Regex.Count -gt 0
+            StartCount = $StartsWith.count -gt 0
+            EndCount   = $EndsWith.count -gt 0
+        } | ft -auto | out-string | Join-string -sep "`n"  | write-host
+
+        $query =
+            $query | Where-Object {
+                $curFile = $_
+                # Conditionally compare against a list of regexes
+                # only include files that match 1 or more of the patterns
+                # finally emits 0 or 1 boolean
+                $tests = @(
+                    if($Regex.Count -gt 0) {
+                        @( $Regex ).Where(
+                            { $curFile.Name -match $_  }, 'first', 1 )
+                    }
+
+                    if($StartsWith.count -gt 0) {
+                        @( $StartsWith ).Where(
+                            { $curFile.BaseName -match '^' + $_  }, 'first', 1)
+                    }
+                    if($EndsWith.count -gt 0) {
+                        @( $EndsWith ).Where(
+                            { $curFile.BaseName -match $_ + '$'  }, 'first', 1)
+                    }
+                )
+                $tests.where({ $_ }, 'first', 1)
+            }
+        'Filtered down to {0} files, using the regex: {1}, RegexStart: {2}, RegexEnd: {3}' -f @(
+                $query.count
+                $Regex      ?? '' | Join-String -sep ', ' -SingleQuote
+                $StartsWith ?? '' | Join-String -sep ', ' -SingleQuote
+                $EndsWith   ?? '' | Join-String -sep ', ' -SingleQuote
+
+            ) | Write-host -fg $Color.Fg
+    }
+
+    if($query.count -eq 0 ) { write-error 'Zero matches found after applying filters' }
+
+    $query
+}
+
 function Convert-PqLibTableFromPaste {
     <#
     .SYNOPSIS
@@ -102,8 +199,6 @@ function Update-PqLibBestPracticeAnalyzerRules {
         [Parameter()]
         [string]$DestinationPath
     )
-
-
 @'
 Sources:
   - https://raw.githubusercontent.com/microsoft/Analysis-Services/master/BestPracticeRules/BPARules.json
