d7917f5d641f70cfc4d9233d3bba1993e2cd8f4d
Jake Bolton <ninmonkeys@gmail.com>
2024-06-03 17:40:48 -0500

    move files

---
+++ source-pwsh/old-example-src/format-pqlibDate.ps1
@@ -0,0 +1,35 @@
+﻿'Original command'
+$d = [datetime]'2024-03-13'
+$cults = Get-culture -ListAvailable
+$cults | %{
+    $Cult = $_
+    $d.ToString( $_.DateTimeFormat.ShortDatePattern, $cult )
+    #  .DateTimeFormat.ShortDatePattern
+    #   $d.ToShortDateString() } )
+}
+
+function Format-PqLibDate {
+
+    param(
+        [ValidateSet(
+            'ShortDate', 'ShortTime'
+        )]
+        [Alias('Template', 'FStr')]
+        [Parameter(Mandatory)]
+        [string]$Format
+    )
+
+    $FormatStr = switch($Format) {
+        'ShortDate' { 'yyyy-MM-dd' }
+        'ShortTime' { 'HH:mm:ss' }
+        default { throw "ShouldNeverReachException: Unhandled Format '$Format'" }
+    }
+
+    $d = [datetime]'2024-03-13'
+$cults = Get-culture -ListAvailable
+$cults | %{
+    $d.ToString( $_.DateTimeFormat.ShortDatePattern,(get-culture 'de' ))
+    #  .DateTimeFormat.ShortDatePattern
+    #   $d.ToShortDateString() } )
+}
+}
