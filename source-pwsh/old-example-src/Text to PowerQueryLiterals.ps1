8fa9078a763ddd8a7c3c0828d7911d690393cb6d
Jake Bolton <ninmonkeys@gmail.com>
2024-06-05 01:43:52 -0500

    Add functions: Word wrap, cleanup

---
+++ source-pwsh/old-example-src/Text to PowerQueryLiterals.ps1
@@ -0,0 +1,55 @@
+class RuneToPqLiteral {
+    [string] $Source
+    [int] $Codepoint
+    [string] $Hex
+
+    # hidden $First
+
+    hidden [string] $DisplayCodepoint
+
+    RuneToPqLiteral ( [string] $Rune ) {
+        $first, $rest = $Rune.EnumerateRunes()
+        if ($rest.count -gt 0) { throw 'nyi IEnumerable list of runes' }
+
+        $This.Source          = $Rune
+        # $This.First           = $first
+        $This.Codepoint       = $first.Value
+        $This.Hex             = '{0:x}' -f @($This.Codepoint)
+        $This.DisplayCodepoint = '0x' + $this.Hex
+        # just do 1 for now
+        # foreach($Rune in $InputText.EnumerateRunes()) {
+
+        # }
+        $null -eq 2
+    }
+
+    [string] ToString() {
+        return ''
+    }
+
+    [int] Length () {
+        return $this.Source.EnumerateRunes().Value.Count
+    }
+
+
+}
+
+function StringToPqLiteral {
+    param(
+        [Parameter(ValueFromPipeline)]
+        $InputText
+    )
+
+    process {
+        # $ErrorActionPreference = 'break'
+        $InputText.EnumerateRunes() | ForEach-Object {
+            [RuneToPqLiteral]::new($_)
+        }
+        # $ErrorActionPreference = 'continue'
+    }
+}
+
+
+$sample = '    🙊🐛🐈'
+
+StringToPqLiteral $sample
